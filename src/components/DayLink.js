44f9c2c84483fbe3759853e0830020cc5cc55ae9
Jessica Wray <jessica@thrivetrm.com>
2017-12-09 13:07:40 -0500

    Sort time as a number and not a string, duh

--- src/components/DayLink.js
+++ src/components/DayLink.js
@@ -4,13 +4,15 @@ import { Button } from 'react-bootstrap';
 import { withHandlers } from 'recompose';
 
 const DayLink = ({ entry, handleClick, active }) => (
-    <Button
-      className='DayLink-button'
-      onClick={handleClick}
-      active={active}
-    >
+  <Button
+    className='DayLink-button'
+    onClick={handleClick}
+    active={active}
+  >
+    <div>
       {moment(entry.time).format('ll')}
-    </Button>
+    </div>
+  </Button>
 );
 
 export default withHandlers({
--- src/components/View.js
+++ src/components/View.js
@@ -4,43 +4,47 @@ import DayLink from './DayLink';
 import DayView from './DayView';
 import Averages from './Averages';
 
-const View = ({ data, onClick, selectedTime }) => (
-  <div className="App">
-    <div className="App-header lead">
-      react mileage tracking
-    </div>
-    <div>Built with Node.js and React</div>
-    <div>Twilio | AWS Lambda | API Gateway | DynamoDB</div>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-sm-2 col-xs-12'>
-          <h4>Dates</h4>
-          {data.map(entry => (
-            <DayLink
-              entry={entry}
-              key={entry.time}
-              onClick={onClick}
-              active={selectedTime === entry.time}
-            />
-          ))}
-        </div>
-        <DayView
-          entry={data.find(entry => entry.time === selectedTime)}
-          className='col-sm-5 col-xs-6'
-        />
-        <Averages
-          data={data}
-          className='col-sm-5 col-xs-6'
-        />
+const View = ({ data, onClick, selectedTime }) => {
+  return (
+    <div className="App">
+      <div className="App-header lead">
+        react mileage tracking
       </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-2 col-xs-12'>
+            <h4>Dates</h4>
+            {data.map(entry => (
+              <DayLink
+                entry={entry}
+                key={entry.time}
+                onClick={onClick}
+                active={selectedTime === entry.time}
+              />
+            ))}
+          </div>
+          <DayView
+            entry={data.find(entry => entry.time === selectedTime)}
+            className='col-sm-5 col-xs-6'
+          />
+          <Averages
+            data={data}
+            className='col-sm-5 col-xs-6'
+          />
+        </div>
+      </div>
+      <div>Built with Node.js and React (<a href='http://github.com/wrayjs/vroombot'>code here</a>)</div>
+      <div>Twilio | AWS Lambda | API Gateway | DynamoDB</div>
     </div>
-  </div>
-);
+  );
+}
 
 
 // export default View;
 export default compose(
-  mapProps(({ data }) => ({ data: data.sort(item => item.time) })),
+  mapProps(({ data }) => ({
+    data: data.sort((a, b) => (a.time - b.time)),
+  })),
   withStateHandlers(({ data }) => {
     console.log(data);
     return {
